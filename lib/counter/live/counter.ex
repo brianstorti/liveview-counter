20e799697eafe8e6b5b5ec40e5695d75306e2538
Brian Thomas Storti <btstorti@gmail.com>
2020-04-17 19:55:09 -0300

    With broadcast

--- lib/counter/live/counter.ex
+++ lib/counter/live/counter.ex
@@ -1,6 +1,8 @@
 defmodule CounterWeb.Counter do
   use Phoenix.LiveView
 
+  @topic "live"
+
   def render(assigns) do
     ~L"""
     <div>
@@ -12,14 +14,32 @@ defmodule CounterWeb.Counter do
   end
 
   def mount(_params, _session, socket) do
+    CounterWeb.Endpoint.subscribe(@topic)
     {:ok, assign(socket, :val, 0)}
   end
 
-  def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+  def handle_event(action = "inc", _, socket) do
+    {:noreply,
+      socket
+      |> update(:val, &(&1 + 1))
+      |> broadcast(action)
+    }
   end
 
-  def handle_event("dec", _, socket) do
-    {:noreply, update(socket, :val, &(&1 - 1))}
+  def handle_event(action = "dec", _, socket) do
+    {:noreply,
+      socket
+      |> update(:val, &(&1 - 1))
+      |> broadcast(action)
+    }
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
+  end
+
+  defp broadcast(socket, action) do
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, action, socket.assigns)
+    socket
   end
 end
