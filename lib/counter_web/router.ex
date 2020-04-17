85fe5cfced307e730e3ea7e2ece4ba29194debd4
Brian Thomas Storti <btstorti@gmail.com>
2020-04-17 19:43:46 -0300

    Working counter

---
+++ lib/counter_web/router.ex
@@ -0,0 +1,26 @@
+defmodule CounterWeb.Router do
+  use CounterWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", CounterWeb do
+    pipe_through :browser
+
+    live("/", Counter, layout: { CounterWeb.LayoutView, "app.html" })
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", CounterWeb do
+  #   pipe_through :api
+  # end
+end
